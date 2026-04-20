e78cd2f0f3927c3cd3436864dce8bb654d3e7452
Michael Bergstrom <mbergstrom5060@floridapoly.edu>
2026-04-20 14:50:23 -0400

    moved login functions to Data.js

--- Data.js
+++ Data.js
@@ -6,17 +6,32 @@ class User {
         this.score = 0;
         this.correctAns = 0;
     }
+}
 
-    login(username, password) {
-        if(this.username == username && this.password == password) {
-            //do logic to go to next page or something can't do this rn
-        }
-        else {
-            //output on html saying incorrect password
-        }
+class main {
+    constructor() {
+        this.userCount = 0;
+        this.users = [];
+        this.users.push(new User("mberg", "1234ddffss&", this.userCount));
+        this.userCount++;
+        this.users.push(new User("dserr", "rreerree4433##ccDDsSdaL", this.userCount));
+        this.userCount++;
+        this.users.push(new User("smurr", "54321ttEDS%$ddd", this.userCount));
+        this.userCount++;
     }
 
-    logout() {
-        //go to login page in some way
+    login() {
+        const inputElement1 = document.getElementById("username");
+        const username = inputElement1.value;
+        const inputElement2 = document.getElementById("password");
+        const password = inputElement2.value;
+        this.userCount++;
+        for(let i = 0; i < this.userCount; i++) {
+            if(username == this.users[i].username && password == this.users[i].password){
+                window.location.replace("MainFile.html");
+            }
+        }
     }
 }
+
+m = new main();

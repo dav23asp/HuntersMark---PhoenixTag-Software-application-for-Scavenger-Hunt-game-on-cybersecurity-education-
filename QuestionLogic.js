5d57ebd0ee8fb985bfbc215836d9a6f55d5f06ed
Michael Bergstrom <mbergstrom5060@floridapoly.edu>
2026-04-23 00:04:58 -0400

    leaderboard shows up better on welcomePage.html and there's a way to go back to welcome page.

--- QuestionLogic.js
+++ QuestionLogic.js
@@ -34,7 +34,15 @@ function hamburger(menuId){
 
 function showLeaderboard(){
     document.getElementById("leaderboard-container").style.display = "block";
-    document.getElementById("question-root").style.display = "none";
+    const question = document.getElementById("question-root");
+    if(question) {
+        question.style.display = "none";
+    }
+    const welcome = document.getElementById("welcome-container");
+    if (welcome) {
+        welcome.style.display = "none";
+    }
+
 
     update(); // re-render leaderboard
 }
--- welcomePage.js
+++ welcomePage.js
@@ -1,1 +1,13 @@
-var nfc = ""
+function showWelcome(){
+    document.getElementById("leaderboard-container").style.display = "none";
+
+    const question = document.getElementById("question-root");
+    if (question) {
+        question.style.display = "none";
+    }
+
+    const welcome = document.getElementById("welcome-container");
+    if (welcome) {
+        welcome.style.display = "block";
+    }
+}
